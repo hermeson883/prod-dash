dcdd3875d404793e006553e63136e279f7f0cd8f
hermeson883 <hermesondovalebeserra722@gmail.com>
2025-12-24 22:56:51 -0300

    refactoring the login page

--- app/api/login.py
+++ app/api/login.py
@@ -4,6 +4,7 @@ from app.models.get_user import LoginFormUser
 from classes.database import SessionLocal
 from classes.users import Users
 from typing import Annotated
+from app.api.jwt_token.jwt_util import JWTUtil
 
 app = FastAPI()
 
@@ -17,15 +18,23 @@ app.add_middleware(
 
 @app.post("/login")
 async def login(user_login_data: Annotated[LoginFormUser, Form()]):
-    print("recebi a requisicao")
-    db = SessionLocal()
-    user = db.query(Users).filter(user_login_data.username == Users.username).first()
 
+    db = SessionLocal()
+    user = db.query(Users).filter(Users.username == user_login_data.username).first()
 
     if not user:
-        return HTTPException(
+        raise HTTPException(
             status.HTTP_404_NOT_FOUND,
             detail="Usuário inexistente"
         )
 
-    return user
+    
+    jwt_helper = JWTUtil()
+
+    encoded_data = jwt_helper.create_encode(user_login_data.username)
+
+    return {
+        "username": user.username,
+        "token": encoded_data["token"],
+        "expired_date": encoded_data["exp"]
+    }
--- app/assets/js/login.js
+++ app/assets/js/login.js
@@ -1,29 +1,34 @@
 async function handleFormData(event) {
-    event.preventDefault(); // ESSENCIAL
+    event.preventDefault()
 
     // Sempre o <form>, nunca o botão
-    const form = event.currentTarget;
-    const formdata = new FormData(form);
+    const form = event.currentTarget
+    const formdata = new FormData(form)
 
     try {
         const response = await fetch("http://127.0.0.1:8000/login", {
             method: "POST",
             body: formdata
-        });
+        })
         if (!response.ok) {
-            alert("Usuário ou senha inválidos");
-            return;
+            alert("Usuário ou senha inválidos")
+            return
         }
 
-        const result = await response.json();
-        window.location.href = "/dashboard";
+        const result = await response.json()
+
+        // Salva o token e username
+        localStorage.setItem("username", result.username)
+        localStorage.setItem("token", result.token)
+
+        window.location.href = "/dashboard"
 
     } catch (error) {
-        alert("Estamos com problemas técnicos");
-        console.error(error);
+        alert("Estamos com problemas técnicos")
+        console.error(error)
     }
 }
 
 document
     .querySelector("#loginForm")
-    ?.addEventListener("submit", handleFormData);
+    ?.addEventListener("submit", handleFormData)
